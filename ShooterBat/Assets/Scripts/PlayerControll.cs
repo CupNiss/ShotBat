c80d0fc6293b2e3c89e5d32bb431651db6d2f343
CupNiss <cupniss@gmail.com>
2025-08-28 19:05:01 -0300

    Adicionado Acelerometro

--- ShooterBat/Assets/Scripts/PlayerControll.cs
+++ ShooterBat/Assets/Scripts/PlayerControll.cs
@@ -6,16 +6,25 @@ using UnityEngine.SceneManagement;
 public class PlayerControll : MonoBehaviour
 {
     public float moveSpeed;
+    public float limitX = 5.0f;
+    private float tiltAmount = 30.0f;
+    private float tiltSmooth = 5;
+
+    private bool invertControl = false;
+    private Vector3 targetPosition;
+
     Rigidbody2D rb2D;
 
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        targetPosition = transform.position;
     }
 
     void Update()
     {
         TouchController();
+        Gyro();
     }
 
     private void TouchController()
@@ -39,6 +48,29 @@ public class PlayerControll : MonoBehaviour
         }
     }
 
+    void Gyro()
+    {
+        float tilt = Input.acceleration.x;
+
+        if (invertControl)
+        {
+            tilt = -tilt;
+        }
+
+        float moveX = tilt * moveSpeed * Time.deltaTime;
+
+        targetPosition.x = Mathf.Clamp(transform.position.x + moveX, -limitX, limitX);
+        targetPosition.y = transform.position.y;
+        targetPosition.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(targetPosition, targetPosition, moveSpeed * Time.deltaTime);
+
+        float targetTilt = -tilt * tiltAmount;
+
+        Quaternion tiltRotation = Quaternion.Euler(0.0f, 0.0f, targetTilt);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, tiltRotation, tiltAmount);
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
